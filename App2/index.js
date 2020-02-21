9353d774955c19ecaa396e685d1ef72afb616a0a
hothdung <ho.thdung@gmail.com>
2020-02-21 16:40:50 +0900

    first projects

---
+++ App2/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = React.createElement('div',null,'hello world')
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)
