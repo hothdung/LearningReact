1628abfe9a6d640c61386ba01c1c3c05c74824bc
hothdung <33243839+hothdung@users.noreply.github.com>
2020-03-02 23:51:35 +0900

    loading data from external json file

---
+++ app6/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PostList from './PostList'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <PostList />
+      </div>
+    );
+  }
+}
+export default App;
